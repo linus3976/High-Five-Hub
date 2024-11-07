8d157cd83d9238f57366c601d9fd036e8afbc501
Colette Timothee <timothee.colette@student-cs.fr>
2024-11-07 17:53:19 +0100

    new functionality : can now start and finsih(almost... waiting for Phil) in the middle of a line

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def parse_arguments():
     """Parse command-line arguments for grid parameters."""
     parser = argparse.ArgumentParser(description="Car navigation parameters")
     parser.add_argument("--size", type=int, required=True, help="Size of the grid (N)")
-    parser.add_argument("--start", type=int, nargs=2, required=True, help="Starting coordinates as two integers")
-    parser.add_argument("--end", type=int, nargs=2, required=True, help="Ending coordinates as two integers")
+    parser.add_argument("--start", type=float, nargs=2, required=True, help="Starting coordinates as two integers")
+    parser.add_argument("--end", type=float, nargs=2, required=True, help="Ending coordinates as two integers")
     parser.add_argument("--dir_init", type=int, nargs=2, required=True, help="Initial direction as a tuple of two integers")
 
     args = parser.parse_args()
@@ -43,7 +43,8 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     # Parse arguments from the terminal
-    size, start, end, dir_init = parse_arguments()
+    size, start_float, end_float, dir_init = parse_arguments()
+    start, end = int(start_float), int(end_float)
 
     # Initialize motor controller and line follower with motor control function
     motor_controller = Urkab()
@@ -60,12 +61,84 @@ if __name__ == '__main__':
     camera.framerate = 32
     raw_capture = PiRGBArray(camera, size=camera.resolution)
     sleep(0.1)  # Allow the camera to warm up
+    
+    #correcting start
+    if (start_float[0] - start[0] > 0.1) or (star_float[1] - start[1] > 0.1):
+        start = (int(start_float[0] + dir_init[0]),int(start_float[1] + dir_init[1]))
+ 
+    if (end_float[0] - end[0] > 0.1) or (end_float[1] - end[1] > 0.1):
+        #calculating the final direction once the final node is reached normally
+        final_dir = (int((end_float[0] - end[0])*2),int((end_float[1] - end[1])*2))
 
     # Initialize the itinerary
     g = grid_to_adjacency_matrix(size)
     itin = bfs_with_edges_from_matrix(g, start, end, size)
-    dir_l = dir_list(dir_list_absolute(itin), dir_init)
+    l = dir_list_absolute(itin)  
+    if (end_float[0] - end[0] > 0.1) or (end_float[1] - end[1] > 0.1):
+        l.append(final_dir)
+        dir_l = dir_list(l, dir_init)
+        final_step = dir_l.pop()
+    else:
+        dir_l = dir_list(l, dir_init)
+        final_step = ""
     logging.info(f"Initial itinerary: {dir_l}")
+
+    # Initialize intersection tracking
+    intersection_detected = False
+    previous_intersection = False
+    frames_without_intersection = 0  # Counter for consecutive frames without intersection
+    direction_index = 0
+   
+#going to the next intersection if the starting position is in the middle of a line
+
+    #checking if we're in the middle of a line
+    if (start_float[0] - start[0] > 0.1) or (star_float[1] - start[1] > 0.1):
+        #going to the next intersection
+        try:
+            #same method as the thing just under
+            for frame in camera.capture_continuous(raw_capture,format="bgr",use_video_port=True):
+                image = frame.array
+                intersection_detected = detect_intersections(image)
+                
+                #checking for intersection
+                if intersection_detected:
+                    if not previous_intersection :
+                        frames_without_intersection = 0
+                        previous_intersection = True
+                if (not intersection_detected) and previous_intersection:
+                    frames_without_intersection += 1
+
+                    #presence of two consecutive frames
+                    if frames_without_intersection>1:
+                        previous_intersection = False
+                        frames_without_intersection = 0
+                        direction_index +=1
+                        if direction_index > 0 :
+                            #stop the try
+                            motor_controller.carStop()
+                            break
+                        else:
+                            motor_controller.executeDirection("straight")
+                
+                #processing the frame
+                processed_frame = line_follower.process_frame(image)
+                #PID based on the results
+                motor_left, motor_right = PID_control.update(delta_time, line_follower.get_attributes())
+                line_follower.apply_control(motor_left, motor_right, motor_controller)
+
+                current_time = time.perf_counter()
+                delata_t = current_time - previous_time
+                previous_time = current_time
+
+        finally : 
+            #just stops the car, doesn't do anything else, and I don't really think it should
+            motor_controller.carStop()
+
+        #removing the brake status again
+        motor_controller.carDeactivateEmergencyStop()
+    
+    previous_time = time.perf_counter()
+    delta_time = 0.1
 
     # Initialize intersection tracking
     intersection_detected = False
@@ -142,6 +215,11 @@ if __name__ == '__main__':
             raw_capture.truncate(0)
 
     finally:
+        #do the final step
+        motor_controller.executeDirection(final_step)
+        if final_step != "" :
+            #move forward a certain amount
+
         # Release resources and stop the car
         cv2.destroyAllWindows()
         motor_controller.carStop()
