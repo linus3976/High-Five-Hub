0fdc8dd673aaba56be537ec9f0c2c091066b596b
Philpp <phs@mail.com>
2024-11-07 17:56:43 +0100

    test script

---
+++ test_turn.py
@@ -0,0 +1,23 @@
+from car_lib import *
+import logging
+
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+# Initialize the Urkab
+logging.debug("Initializing Urkab...")
+urkab = Urkab()
+
+# Turn right
+logging.debug("Turning right...")
+urkab.executeDirection("right")
+sleep(2)
+
+# Turn left
+logging.debug("Turning left...")
+urkab.executeDirection("left")
+sleep(2)
+
+# Do a 180 turn
+logging.debug("Doing a 180 turn...")
+urkab.executeDirection("do_a_flip")
+
+logging.debug("Done!")
