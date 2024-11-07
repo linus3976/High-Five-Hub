9e68bd2ebf0ce2261eb1a1491148e11c2fb3ffbf
Philpp <phs@mail.com>
2024-11-07 19:55:23 +0100

    add try, change a bit

--- calibrate_system.py
+++ calibrate_system.py
@@ -6,7 +6,7 @@ from croisement import detect_intersections
 from time import perf_counter, sleep
 
 def initilize():
-    global camera, raw_capture, urkab, intersection_detected, previous_intersection, frames_without_intersection
+    global camera, raw_capture, urkab
     # Initialize the PiCamera
     camera = PiCamera()
     camera.resolution = (160, 128)
@@ -17,12 +17,12 @@ def initilize():
     urkab = Urkab()
     urkab.carResetEmergencyStop()
 
-    # Initialize intersection tracking
-    intersection_detected = False
+def calibrate_turning():
+    global camera, raw_capture, urkab
+    # Initialize variables tracking
     previous_intersection = False
     frames_without_intersection = 0  # Counter for consecutive frames without intersection
 
-def calibrate_turning():
     urkab.carTurnRight(250,250)
     start_time = perf_counter()
     lost_intersection = False
@@ -57,11 +57,22 @@ def calibrate_turning():
     return end_time - start_time
 
 def main():
-    print("HighFive Calibration system: Place Urkab in front of an intersection")
-    input("Press enter to start calibration")
-    initilize()
-    time_to_turn = calibrate_turning()
-    print(f"Time to turn: {time_to_turn}")
+    try:
+        print("HighFive Calibration system: Place Urkab in front of an intersection")
+        input("Press enter to start calibration")
+        initilize()
+        time_to_turn = calibrate_turning()
+        print(f"Time to turn: {time_to_turn}")
+    except KeyboardInterrupt:
+        urkab.carStop()
+        camera.close()
+        print("Calibration stopped by user")
+        exit()
+    finally:
+        # Release resources and stop the car
+        cv2.destroyAllWindows()
+        motor_controller.carStop()
+        motor_controller.carDisconnect()
 
 if __name__ == "__main__":
     main()
