b0b2de1ed6171179d6e099e1263f880b7b8cce1d
Eckey Linus Bennet <linus.eckey@student-cs.frv>
2024-11-08 12:15:10 +0100

    loading float

--- car_lib.py
+++ car_lib.py
@@ -11,7 +11,7 @@ try:
     load_dotenv(env_file_path)
 
     # Access the variable
-    TURNING_CONST = os.getenv("TIME_TO_TURN")
+    TURNING_CONST = float(os.getenv("TIME_TO_TURN"))
     logging.debug(f"The value of TIME_TO_TURN is: {TURNING_CONST}")
 except ValueError:
     TURNING_CONST = 2.8
